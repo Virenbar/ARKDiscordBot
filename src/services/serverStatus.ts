1b6025c49d15930a5e8d4c5d5a05df069501aef4
Virenbar <Virenbar@users.noreply.github.com>
2024-05-09 17:44:01 +0500

    Remove broken command

--- src/services/serverStatus.ts
+++ src/services/serverStatus.ts
@@ -5,21 +5,20 @@ import type { Service } from "./index.js";
 const Logger = log4js.getLogger("Server Status");
 let Client: ARKBot;
 let API: string;
-export const Servers: ARKServer[] = [];
-export const Players: Player[] = [];
+export const ServersPVP: ARKServer[] = [];
+export const ServersPVE: ARKServer[] = [];
 
 function initialize(client: ARKBot) {
     Client = client;
 }
 
 function reload() {
-    Servers.length = 0;
+    ServersPVP.length = 0;
     for (const server of Client.config.PVP.servers) {
-        Servers.push({
+        ServersPVP.push({
             name: server.name,
             ip: "",
             port: 1000,
-            maxplayers: 0,
             players: 0,
             status: false,
             id: server.id,
@@ -33,17 +32,8 @@ async function refresh() {
     try {
         const Result = await fetch(API);
         const status = await Result.json() as Status;
-        Players.length = 0;
-        Players.push(...status.players);
-        Servers.forEach(S => {
-            const server = status.servers.find(s => s.id == S.id);
-            if (!server) { return; }
-            S.ip = server.ip;
-            S.port = server.port;
-            S.maxplayers = server.maxplayers;
-            S.players = server.players;
-            S.status = server.status;
-        });
+        refreshServers(ServersPVP, status.cluster_pvp);
+        refreshServers(ServersPVE, status.cluster_pve);
         Logger.debug("Status updated");
     } catch (error) {
         Logger.error("Status update failed");
@@ -51,27 +41,33 @@ async function refresh() {
     }
 }
 
+function refreshServers(servers: ARKServer[], cluster: Server[]) {
+    servers.forEach(S => {
+        const server = cluster.find(s => s.id == S.id);
+        if (!server) { return; }
+        S.ip = server.ip;
+        S.port = server.port;
+        S.players = server.players;
+        S.status = server.status;
+    });
+}
+
 const name = "Server Status";
 const Service: Service = { name, initialize, reload };
-const ServerStatus = { ...Service, refresh, Servers, Players } as const;
+const ServerStatus = { ...Service, refresh, ServersPVP, ServersPVE } as const;
 export default ServerStatus;
 
 export interface Status {
-    servers: Server[]
-    players: Player[]
-}
-
-export interface Player {
-    server: string
-    name: string
-    tribe: string | null
+    cluster_pvp: Server[]
+    cluster_pve: Server[]
+    cluster_pvp_wipe: Server[]
+    cluster_pvp_asa: Server[]
 }
 
 export interface Server {
     name: string
     ip: string
     port: number
-    maxplayers: number
     players: number
     status: boolean
     id: number
@@ -80,4 +76,3 @@ export interface Server {
 export interface ARKServer extends Server {
     battlemetrics?: string
 }
-
